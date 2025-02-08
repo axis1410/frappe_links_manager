4ca9daf91f365959a8e19fc9ec00be7a1f960cd3
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-08 11:02:06 +0530

    feat: Add Short Link functionality and path resolver integration

--- links_manager/utils.py
+++ links_manager/utils.py
@@ -1,9 +1,14 @@
 from typing import Any, LiteralString
 
+import frappe
 from frappe.website.path_resolver import resolve_path as original_resolve_path
 
 
 def path_resolver(path: str) -> Any | str | LiteralString | None:
 	"""Handle short links"""
+
+	if frappe.db.exists("Short Link", {"short_link": path}):
+		destination = frappe.db.get_value("Short Link", {"short_link": path}, "destination_url")
+		frappe.redirect(destination)
 
 	return original_resolve_path(path)
